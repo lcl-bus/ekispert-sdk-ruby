12cbbf03fe86ab5933b6f94e9b62c8aac4c6b687
pupupopo88 <3654953+pupupopo88@users.noreply.github.com>
2018-04-18 16:10:07 +0900

    add config spec

--- lib/ekispert.rb
+++ lib/ekispert.rb
@@ -2,4 +2,5 @@ require 'faraday'
 require 'faraday_middleware'
 require 'nokogiri'
 
+require 'ekispert/config'
 require 'ekispert/version'
---
+++ spec/ekispert/config_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+RSpec.describe Ekispert::Config do
+  describe 'default config' do
+    it 'return default host' do
+      expect(Ekispert::Config.host).to eq 'https://api.ekispert.jp'
+    end
+    it 'return default version' do
+      expect(Ekispert::Config.version).to eq 'v1'
+    end
+    it 'return default http_proxy' do
+      expect(Ekispert::Config.http_proxy).to eq ENV['HTTP_PROXY']
+    end
+    it 'return default api_key' do
+      expect(Ekispert::Config.api_key).to eq ''
+    end
+  end
+  describe '#list' do
+    it 'return config list' do
+      expect(Ekispert::Config.list).to match(
+        {
+          :@host => 'https://api.ekispert.jp',
+          :@version => 'v1',
+          :@http_proxy => nil,
+          :@api_key => ''
+        }
+      )
+    end
+  end
+  describe '#set' do
+    it 'can set the config with a block' do
+      config = Ekispert::Config.set do |c|
+        c.host = 'https://example.com'
+        c.version = 'v2'
+        c.http_proxy = 'http://example.com:8080'
+        c.api_key = 'test'
+      end
+      expect(Ekispert::Config.list).to match(
+        {
+          :@host => 'https://example.com',
+          :@version => 'v2',
+          :@http_proxy => 'http://example.com:8080',
+          :@api_key => 'test'
+        }
+      )
+    end
+  end
+end
