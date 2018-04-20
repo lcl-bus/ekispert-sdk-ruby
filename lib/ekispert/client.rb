1d81727ba3c698172b117132bc8bd30e3b1b2ce7
pupupopo88 <3654953+pupupopo88@users.noreply.github.com>
2018-04-20 11:58:36 +0900

    return faraday options for 'Faraday.new'

--- lib/ekispert/client.rb
+++ lib/ekispert/client.rb
@@ -1,4 +1,13 @@
 module Ekispert
   class Client
+    def self.connection_options
+      {
+        url: "#{Config.host}/#{Config.version}/xml",
+        proxy: Config.http_proxy,
+        headers: { 'Content-Type' => 'application/xml;charset=utf-8' },
+        params: { key: Ekispert::Config.api_key },
+        request: { timeout: 5, open_timeout: 2 }
+      }
+    end
   end
 end
--- spec/ekispert/client_spec.rb
+++ spec/ekispert/client_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe Ekispert::Client do
+  describe '.connection_options' do
+    it 'return default options' do
+      expect(Ekispert::Client.connection_options).to match(
+        {
+          url: 'https://api.ekispert.jp/v1/xml',
+          proxy: ENV['http_proxy'],
+          headers: { 'Content-Type' => 'application/xml;charset=utf-8' },
+          params: { key: '' },
+          request: { timeout: 5, open_timeout: 2 }
+        }
+      )
+    end
+  end
 end
