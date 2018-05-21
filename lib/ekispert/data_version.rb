4955a240722183a756ce903b392a651a13c32165
pupupopo88 <3654953+pupupopo88@users.noreply.github.com>
2018-05-21 18:11:12 +0900

    Make use of Ekispert::Util medule methods

--- lib/ekispert/data_version.rb
+++ lib/ekispert/data_version.rb
@@ -22,7 +22,7 @@ module Ekispert
         elem_name = element.name.to_sym
         next unless self.constants.include?(elem_name)
         subclass_instance = create_subclass_instance(element, elem_name)
-        set_instance_variable(data_version, subclass_instance)
+        Ekispert::Util.set_instance_variable(data_version, subclass_instance)
       end
       data_version
     end
@@ -32,44 +32,13 @@ module Ekispert
       instance = self.const_get(subclass_name).new
       # attribute
       if element.attributes.size > 0
-        set_methods_from_attributes(element.attributes, instance)
+        Ekispert::Util.set_methods_from_attributes(element.attributes, instance)
       end
       # text node
       if (child_elem = element.children[0]) && child_elem.text?
-        set_method_from_text(child_elem.text, instance)
+        Ekispert::Util.set_method_from_text(child_elem.text, instance)
       end
       instance
-    end
-
-    def self.set_methods_from_attributes(attributes, instance)
-      attributes.each do |name, attribute|
-        instance.class.class_eval { attr_accessor name }
-        instance.instance_variable_set("@#{name}", attribute.value)
-      end
-    end
-
-    def self.set_method_from_text(text, instance)
-      instance.class.class_eval { attr_accessor :text }
-      instance.instance_variable_set('@text', text)
-      instance.instance_eval { alias :to_s :text }
-    end
-
-    # Ex.
-    # #<Ekispert::DataVersion @version_list=[], @copyrights_list=[]>
-    # -> @version_list=[
-    #      #<Ekispert::DataVersion::Version @...>,
-    #      <Ekispert::DataVersion::Version @...>
-    #    ],
-    # -> @copyrights_list=[
-    #      #<Ekispert::DataVersion::Copyrights @...>,
-    #      <Ekispert::DataVersion::Copyrights @...>
-    #    ],
-    def self.set_instance_variable(base_instance, instance)
-      if instance.is_a?(Ekispert::DataVersion::Version)
-        base_instance.version_list << instance
-      elsif instance.is_a?(Ekispert::DataVersion::Copyrights)
-        base_instance.copyrights_list << instance
-      end
     end
   end
 end
