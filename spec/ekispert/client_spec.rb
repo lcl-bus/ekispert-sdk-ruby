3926a5cf23b42164f578076a606af75f39055edd
pupupopo88 <3654953+pupupopo88@users.noreply.github.com>
2018-06-11 17:01:15 +0900

    Improve Client spec
    .raise_error is Client class method

--- spec/ekispert/client_spec.rb
+++ spec/ekispert/client_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
+require 'helper/util_helper'
 
 RSpec.describe Ekispert::Client do
   include Ekispert::SpecHelper::Config
+  include UtilHelper
   before { set_ekispert_default_config }
   describe '.connection_options' do
     it 'return default options' do
@@ -39,13 +41,65 @@ RSpec.describe Ekispert::Client do
     let(:res) { Ekispert::Client.get('dataversion') }
     context 'request has succeeded（request dataversion）' do
       it 'should return Nokogiri::XML::NodeSet instance' do
-        expect(res.class).to eq  Nokogiri::XML::NodeSet
+        expect(res.class).to eq Nokogiri::XML::NodeSet
       end
     end
     context 'api_key = nil' do
       before { set_ekispert_config(api_key: nil) }
       it 'should raise Ekispert::Error::Forbidden' do
         expect { res }.to raise_error(Ekispert::Error::Forbidden)
+      end
+    end
+  end
+  describe '.raise_error' do
+    let(:stub) do
+      Faraday.new do |conn|
+        conn.adapter :test, Faraday::Adapter::Test::Stubs.new do |stub|
+          stub.get('https://api.ekispert.jp') { [status_code, {}, read_xml('client_error.xml')] }
+        end
+      end
+    end
+    let(:error) { Ekispert::Client.send(:raise_error, stub.get('https://api.ekispert.jp')) }
+    context 'status code 1' do
+      let(:status_code) { 1 }
+      it 'should raise Error::InternalError' do
+        expect { error }.to raise_error(Ekispert::Error::InternalError)
+      end
+    end
+    context 'status code 400' do
+      let(:status_code) { 400 }
+      it 'should raise Ekispert::Error::BadRequest' do
+        expect { error }.to raise_error(Ekispert::Error::BadRequest)
+      end
+    end
+    context 'status code 403' do
+      let(:status_code) { 403 }
+      it 'should raise Ekispert::Error::Forbidden' do
+        expect { error }.to raise_error(Ekispert::Error::Forbidden)
+      end
+    end
+    context 'status code 404' do
+      let(:status_code) { 404 }
+      it 'should raise Error::NotFound' do
+        expect { error }.to raise_error(Ekispert::Error::NotFound)
+      end
+    end
+    context 'status code 499' do
+      let(:status_code) { 499 }
+      it 'should raise Error::ClientError' do
+        expect { error }.to raise_error(Ekispert::Error::ClientError)
+      end
+    end
+    context 'status code 500' do
+      let(:status_code) { 500 }
+      it 'should raise Error::ServerError' do
+        expect { error }.to raise_error(Ekispert::Error::ServerError)
+      end
+    end
+    context 'status code 599' do
+      let(:status_code) { 599 }
+      it 'should raise Error::ServerError' do
+        expect { error }.to raise_error(Ekispert::Error::ServerError)
       end
     end
   end
