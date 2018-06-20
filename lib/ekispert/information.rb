b72b785874bf9b4f334a8866fa84f0c0c0b3710d
pupupopo88 <3654953+pupupopo88@users.noreply.github.com>
2018-06-20 16:47:00 +0900

    Move Ekispert::Information::Exit from Information class file
    and add spec

--- lib/ekispert/information.rb
+++ lib/ekispert/information.rb
@@ -30,16 +30,6 @@ module Ekispert
       class Comment < EkispertBase; end
     end
 
-    class Exit < EkispertBase
-      attr_reader :name_list, :comment_list
-      def initialize(element)
-        @name_list = []
-        @comment_list = []
-        super(element)
-      end
-      class Name < EkispertBase; end
-      class Comment < EkispertBase; end
-    end
     class Type < EkispertBase; end
 
     private
