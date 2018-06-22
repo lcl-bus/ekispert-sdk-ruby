9845061556e09e1d8ec5a4c607ecd3d4198e135b
hmaruyama <hmaruyama@users.noreply.github.com>
2018-06-22 16:03:26 +0900

    add station light API

--- lib/ekispert/point.rb
+++ lib/ekispert/point.rb
@@ -9,18 +9,17 @@ module Ekispert
       super(element)
     end
 
-    # request to '/station' or '/station/light'
-    # and parse Point class
+    # parse Point class
     def self.get_station(**params)
-      path = station_request_path(params)
-      to_point(Ekispert::Client.get(path, params))
+      to_point(Ekispert::Client.get('station', params))
+    end
+
+    def self.get_station_light(**params)
+      binding.pry
+      to_point(Ekispert::Client.get('station/light', params))
     end
 
     private
-
-    def self.station_request_path(**request_params)
-      Station::PAID_REQUEST_PARAMS.any? { |param| request_params.key?(param) } ? 'station' : 'station/light'
-    end
 
     def self.to_point(elem_arr)
       elem_arr.xpath('//Point').map { |elem| self.new(elem) }
