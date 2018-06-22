38886647026fcdb31c0f41c02930da62ad5a8e4e
pupupopo88 <3654953+pupupopo88@users.noreply.github.com>
2018-06-22 13:55:36 +0900

    Add Information::Exit#name and #comment
    alias method

--- spec/ekispert/information/exit_spec.rb
+++ spec/ekispert/information/exit_spec.rb
@@ -19,6 +19,16 @@ RSpec.describe Ekispert::Information::Exit do
             expect(information_list[2].exit_list[0].comment_list[0].class).to eq Ekispert::Information::Exit::Comment
           end
         end
+        describe '#name' do
+          it 'should return "東口"' do
+            expect(information_list[2].exit_list[0].name).to eq '東口'
+          end
+        end
+        describe '#comment' do
+          it 'should return "西原１－３丁目上原１丁目元代々木町"' do
+            expect(information_list[2].exit_list[0].comment).to eq '西原１－３丁目上原１丁目元代々木町'
+          end
+        end
       end
     end
   end
