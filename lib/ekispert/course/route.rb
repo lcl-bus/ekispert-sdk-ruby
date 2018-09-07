786a15ab7e569863e1046ac38e3451bcad9e63ec
pupupopo88 <3654953+pupupopo88@users.noreply.github.com>
2018-09-07 17:19:32 +0900

    Add Ekispert::Course::Route#departure_time and #arrival_time
    サマリーとして表示する場合にあると便利

--- lib/ekispert/course/route.rb
+++ lib/ekispert/course/route.rb
@@ -8,6 +8,14 @@ module Ekispert
         @point_list = []
         super(element)
       end
+
+      def departure_time
+        @line_list[0].departure_state_list[0].datetime
+      end
+
+      def arrival_time
+        @line_list[-1].arrival_state_list[0].datetime
+      end
     end
   end
 end
