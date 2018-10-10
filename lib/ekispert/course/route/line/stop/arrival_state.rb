43a1de1ba588793cef812a3e8f4655cf4dad45ab
Yuki-Iizuka <19604760+Yuki-Iizuka@users.noreply.github.com>
2018-10-10 13:40:13 +0900

    Add Ekispert::Course::Route::Line::Stop::ArrivalState class

---
+++ lib/ekispert/course/route/line/stop/arrival_state.rb
@@ -0,0 +1,20 @@
+module Ekispert
+  class Course < EkispertBase
+    class Route < EkispertBase
+      class Line < EkispertBase
+        class Stop < EkispertBase
+          class ArrivalState < EkispertBase
+            attr_accessor :datetime_list
+
+            def initialize(element)
+              @datetime_list = []
+              super(element)
+            end
+
+            class Datetime < EkispertBase; end
+          end
+        end
+      end
+    end
+  end
+end
