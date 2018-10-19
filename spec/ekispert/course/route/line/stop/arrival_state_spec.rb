a35067efc47e900e9ef98ba5146896f0b07380c5
Yuki-Iizuka <19604760+Yuki-Iizuka@users.noreply.github.com>
2018-10-19 16:21:53 +0900

    Add test about datetime to Stop::DepartureState and Stop::ArrivalState spec

--- spec/ekispert/course/route/line/stop/arrival_state_spec.rb
+++ spec/ekispert/course/route/line/stop/arrival_state_spec.rb
@@ -14,4 +14,9 @@ RSpec.describe Ekispert::Course::Route::Line::Stop::ArrivalState do
       expect(arrival_state.datetime_list[0].class).to eq Ekispert::Course::Route::Line::Stop::ArrivalState::Datetime
     end
   end
+  describe '#datetime' do
+    it 'should return correct value' do
+      expect(arrival_state.datetime.strftime('%Y年%02m月%02d日 %H:%M:%S')).to eq '2018年10月05日 12:57:00'
+    end
+  end
 end
