94db985746187d4d345243af19fc744c1bdfe9c0
pupupopo88 <3654953+pupupopo88@users.noreply.github.com>
2019-01-17 13:05:18 +0900

    Add Ekispert::OperationLine spec. (case of no OperationLine)

--- spec/ekispert/operation_line_spec.rb
+++ spec/ekispert/operation_line_spec.rb
@@ -14,16 +14,25 @@ RSpec.describe Ekispert::OperationLine do
   end
   describe '.to_operation_line' do
     let(:parsed_xml) { Ekispert::Client.send(:parse_xml, xml) }
-    let(:xml) { read_xml('operation_line/akita.xml') }
-    let(:akita_operation_line) { Ekispert::OperationLine.send(:to_operation_line, parsed_xml) }
-    describe '#line_list' do
-      it 'return Array and contains ten elements' do
-        expect(akita_operation_line.line_list.size).to eq 10
+    context 'results not found' do
+      let(:xml) { read_xml('empty_elem.xml') }
+      let(:no_operation_line) { Ekispert::OperationLine.send(:to_operation_line, parsed_xml) }
+      it 'should return nil' do
+        expect(no_operation_line).to be nil
       end
     end
-    describe '#corporation_list' do
-      it 'return Array and contains three elements' do
-        expect(akita_operation_line.corporation_list.size).to eq 3
+    context 'params = { prefectureCode: 5 }' do
+      let(:xml) { read_xml('operation_line/akita.xml') }
+      let(:akita_operation_line) { Ekispert::OperationLine.send(:to_operation_line, parsed_xml) }
+      describe '#line_list' do
+        it 'return Array and contains ten elements' do
+          expect(akita_operation_line.line_list.size).to eq 10
+        end
+      end
+      describe '#corporation_list' do
+        it 'return Array and contains three elements' do
+          expect(akita_operation_line.corporation_list.size).to eq 3
+        end
       end
     end
   end
